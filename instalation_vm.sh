77d5e71084dd28c3883617e037d3f22c9094a8fb
salimmoulouel <salimmouloueletude@outlook.fr>
2021-01-27 11:10:15 +0100

    ajout de fonctionnalité de tunelling de port distant

--- instalation_vm.sh
+++ instalation_vm.sh
@@ -1,3 +1,7 @@
+ssh i- file_pem azureuser@publicipadress
+
+
+
 lspci | grep -i NVIDIA
 CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
@@ -12,3 +16,8 @@ rm -f /tmp/${CUDA_REPO_PKG}
 sudo apt-get update
 
 sudo apt-get install cuda-drivers
+
+
+#build tunelling port between distant machine and local
+#ssh -i key_pem -L localport:localhost:distantport azureuser@publicipadress 
+
